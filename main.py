e778f82a0f0199a5267ce2d232cd9177d9e687dc
Christian Galda <christianuli.galda@stud.tu-darmstadt.de>
2022-05-01 18:58:43 +0200

    partially data analysis stored in txt-file

--- main.py
+++ main.py
@@ -40,6 +40,11 @@ def add_data_fields(raw_data):
     # time diff for complete process
     raw_data['b1_b5_diff'] = (raw_data['b5_timestamp'] - raw_data['b1_timestamp'])
 
+    # waiting times between checkpoints
+    raw_data['b1_b2_diff'] = (raw_data['b2_timestamp'] - raw_data['b1_timestamp'])
+    raw_data['b2_b3_diff'] = (raw_data['b3_timestamp'] - raw_data['b2_timestamp'])
+    raw_data['b3_b4_diff'] = (raw_data['b4_timestamp'] - raw_data['b3_timestamp'])
+    raw_data['b4_b5_diff'] = (raw_data['b5_timestamp'] - raw_data['b4_timestamp'])
     return raw_data
 
 
@@ -61,7 +66,7 @@ def get_basic_analysis(data, type_name):
     print(type_name, 'standard deviation: ', statistics.stdev(data['b1_b5_diff']) / 60)
 
 
-def plot_waiting_times(df, type_name):
+def plot_waiting_time_complete(df, type_name):
     """ plot distribution of complete waiting time for given dataframe """
     df_diff = [x / 60 for x in df['b1_b5_diff']]
     plt.rcParams.update({'figure.figsize': (7, 5), 'figure.dpi': 100})
@@ -72,17 +77,52 @@ def plot_waiting_times(df, type_name):
 
 def plot_arrivals(df, type_name):
     """ plot arrival rate of given dataframe by daytime"""
-    df_diff = [x for x in df['arrival_time']]
+    df_arrivals = [x for x in df['arrival_time']]
     plt.rcParams.update({'figure.figsize': (7, 5), 'figure.dpi': 100})
-    plt.hist(df_diff, bins=24)
+    plt.hist(df_arrivals, bins=24)
     plt.gca().set(title='Ankunft ' + type_name, ylabel='Occurrences')
     plt.show()
 
 
+def plot_waiting_times(df, type_name):
+    """ plot distribution of waiting times between checkpoints"""
+    for i in range(1, 5):
+        df_diff = df['b' + str(i) + '_b' + str(i + 1) + '_diff']
+        df_diff = [x / 60 for x in df_diff]
+        plt.rcParams.update({'figure.figsize': (7, 5), 'figure.dpi': 100})
+        plt.hist(df_diff, bins=150)
+        plt.gca().set(title='Wartezeit zwischen ' + 'b' + str(i) + ' und b' + str(i + 1) + ' für ' + type_name,
+                      ylabel='waiting time')
+        plt.show()
+
+
+def analyze_waiting_times(df, type_name):
+    """ get data analysis for waiting time between checkpoints"""
+    f = open("data_analysis_dump.txt", "a")
+    f.write('*' * 20)
+    f.write('Wartezeiten fuer ' + type_name + ':\n')
+    for i in range(1, 5):
+        df_diff = df['b' + str(i) + '_b' + str(i + 1) + '_diff']
+        df_diff = [x / 60 for x in df_diff]
+        f.write('zwischen ' + 'b' + str(i) + ' und b' + str(i + 1) + '\n')
+        f.write('min: ' + str(min(df_diff)) + '\n')
+        f.write('max: ' + str(max(df_diff)) + '\n')
+        f.write('Durchschnitt: ' + str(statistics.mean(df_diff)) + '\n')
+        f.write('Standardabweichung: ' + str(statistics.stdev(df_diff)) + '\n\n')
+    df_diff = df['b1''_b5''_diff']
+    df_diff = [x / 60 for x in df_diff]
+    f.write('zwischen ' + 'b1 und b5''\n')
+    f.write('min: ' + str(min(df_diff)) + '\n')
+    f.write('max: ' + str(max(df_diff)) + '\n')
+    f.write('Durchschnitt: ' + str(statistics.mean(df_diff)) + '\n')
+    f.write('Standardabweichung: ' + str(statistics.stdev(df_diff)) + '\n\n')
+    f.close()
+
+
 def do_stuff(df, time_name):
     """ output for basic data analysis stuff """
-    # plot_waiting_times(df[df['type'] == 'economy'], 'economy ' + time_name)
-    # plot_waiting_times(df[df['type'] == 'business'], 'business ' + time_name)
+    # plot_waiting_time_complete(df[df['type'] == 'economy'], 'economy ' + time_name)
+    # plot_waiting_time_complete(df[df['type'] == 'business'], 'business ' + time_name)
 
     plot_arrivals(df[df['type'] == 'economy'], 'economy ' + time_name)
     plot_arrivals(df[df['type'] == 'business'], 'business ' + time_name)
@@ -93,6 +133,9 @@ def do_stuff(df, time_name):
 
 
 if __name__ == '__main__':
+    # clear data dump .txt file
+    open("data_analysis_dump.txt", "w").close()
+
     data_frame = pd.read_csv('data.csv', sep=';')
 
     data_frame = cleanup_data(data_frame)
@@ -100,11 +143,13 @@ if __name__ == '__main__':
     data_frame = add_data_fields(data_frame)
 
     # all weekdays
-    data_frame_weekday = data_frame[data_frame.weekday < 5]
-    do_stuff(data_frame_weekday, 'weekday')
+    # data_frame_weekday = data_frame[data_frame.weekday < 5]
+    # do_stuff(data_frame_weekday, 'weekday')
 
     # for weekends
-    data_frame_weekend = data_frame[data_frame.weekday >= 5]
-    do_stuff(data_frame_weekend, 'weekend')
+    # data_frame_weekend = data_frame[data_frame.weekday >= 5]
+    # do_stuff(data_frame_weekend, 'weekend')
 
-    do_stuff(data_frame, 'complete')
+    # do_stuff(data_frame, 'complete')
+    # plot_waiting_times(data_frame, 'alle')
+    analyze_waiting_times(data_frame, 'alle')
